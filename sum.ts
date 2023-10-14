14a90c8f705f480619ce674c71d930344766d795
deepakpokharel <deepakpokharel77@gmail.com>
2023-10-14 16:00:15 -0700

    function

---
+++ sum.ts
@@ -0,0 +1,7 @@
+function addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+  
+  const result = addNumbers(5, 7); // Example usage
+  console.log(`The result of adding 5 and 7 is: ${result}`);
+  
